ba15764b0faf7c98b6069e0fb0948523af1442d1
Kaihiz123 <mk007404@edu.taitotalo.fi>
2025-04-03 16:44:38 +0300

    Slowed fps counter and fixed a bug that shows that first upgrade is buyable at the start

--- Assets/Scripts/UIFPSCounter.cs
+++ Assets/Scripts/UIFPSCounter.cs
@@ -4,8 +4,23 @@ public class UIFPSCounter : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI textField;
 
+    public float interval = 0.5f;
+
+    float timeElapsed = 0f;
+    int frameCount = 0;
+
     private void Update()
     {
-        textField.text = "FPS " + (int)(1f / Time.unscaledDeltaTime);
+        frameCount++;
+        timeElapsed += Time.unscaledDeltaTime;
+        
+        if(timeElapsed > interval)
+        {
+            textField.text = "FPS " + Mathf.RoundToInt((float) frameCount / timeElapsed);
+
+            frameCount = 0;
+            timeElapsed = 0f;
+        }
+        
     }
 }
